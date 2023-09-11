cc106df143615fd343176291a0f6a207f70ff95b
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2023-09-11 10:46:43 -0700

    Connect to API and display movie info

---
+++ src/api/APIVariables.js
@@ -0,0 +1,4 @@
+const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjI3MGZmZmVkNjliYzFkNDdkZTMyNjQ4ZmYwNTBjZCIsInN1YiI6IjY0ZWQwMzNjYzYxM2NlMDEyY2M2YWU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.46gYzA8BqZarsSismrtiSK-mV5olei9Q30Xvvlpwo5A';
+
+const apiKey = 'ef270fffed69bc1d47de32648ff050cd';
+
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "../components/App.css";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
@@ -8,11 +9,31 @@ import PagesTopRated from "../pages/PagesTopRated";
 import PagesNowPlaying from "../pages/PagesNowPlaying";
 import PagesUpcoming from "../pages/PagesUpcoming";
 import PagesFavorites from "../pages/PagesFavorites";
+import {accessToken, apiKey} from "../api/APIVariables";
 
 function AppRouter() {
+  const [movie, setMovie] = useState('');
+  const [picture, setPicture] = useState()
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjI3MGZmZmVkNjliYzFkNDdkZTMyNjQ4ZmYwNTBjZCIsInN1YiI6IjY0ZWQwMzNjYzYxM2NlMDEyY2M2YWU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.46gYzA8BqZarsSismrtiSK-mV5olei9Q30Xvvlpwo5A'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/trending/movie/week?language=en-US', options)
+  .then(response => response.json())
+  .then(response => setMovie(response.results[0].title + ' ' + response.results[0].overview))
+  .then(response => console.log("This is movie", movie))
+  // .then(response => console.log(response))
+  .catch(err => console.error(err));
+  
   return (
     <BrowserRouter>
       <div className="wrapper">
+        <h2>{movie}</h2>
         <Nav>
         <Routes>
           <Route path="/popular" element={<PagesPopular />} />
@@ -23,6 +44,7 @@ function AppRouter() {
         </Routes>
         </Nav>
         <Banner />
+
         <Footer />
       </div>
     </BrowserRouter>
