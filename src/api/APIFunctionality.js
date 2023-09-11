78a57c45f9054a71580949f8393f63f9b305cec9
Taylor Hillier <hilliertaylor7@gmail.com>
2023-09-11 11:46:25 -0700

    moved functionality to seperate file in api folder

--- src/api/APIFunctionality.js
+++ src/api/APIFunctionality.js
@@ -1,4 +1,41 @@
+import { useState, useEffect } from "react";
+
+
 const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjI3MGZmZmVkNjliYzFkNDdkZTMyNjQ4ZmYwNTBjZCIsInN1YiI6IjY0ZWQwMzNjYzYxM2NlMDEyY2M2YWU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.46gYzA8BqZarsSismrtiSK-mV5olei9Q30Xvvlpwo5A';
 
 const apiKey = 'ef270fffed69bc1d47de32648ff050cd';
+  
+function HomeAPI () {
+    const [movies, setMovie] = useState([]);
+  
 
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjI3MGZmZmVkNjliYzFkNDdkZTMyNjQ4ZmYwNTBjZCIsInN1YiI6IjY0ZWQwMzNjYzYxM2NlMDEyY2M2YWU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.46gYzA8BqZarsSismrtiSK-mV5olei9Q30Xvvlpwo5A'
+      }
+    };
+  
+    useEffect(() => {
+      fetch('https://api.themoviedb.org/3/trending/movie/week?language=en-US', options)
+      .then(response => response.json())
+      .then(data => {
+        setMovie(data.results);
+      })
+      .catch(err => console.error(err));
+    }, []);
+
+    return(
+    <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}
+          <img  key={movie.id} src={"https://image.tmdb.org/t/p/w185/" + movie.poster_path}>
+          </img>
+          </li>
+        ))}
+    </ul>
+    );
+}
+
+export default HomeAPI;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,28 +9,9 @@ import PagesTopRated from "../pages/PagesTopRated";
 import PagesNowPlaying from "../pages/PagesNowPlaying";
 import PagesUpcoming from "../pages/PagesUpcoming";
 import PagesFavorites from "../pages/PagesFavorites";
-import {accessToken, apiKey} from "../api/APIFunctionality";
+import HomeAPI from "../api/APIFunctionality";
 
 function AppRouter() {
-  const [movies, setMovie] = useState([]);
-  
-
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjI3MGZmZmVkNjliYzFkNDdkZTMyNjQ4ZmYwNTBjZCIsInN1YiI6IjY0ZWQwMzNjYzYxM2NlMDEyY2M2YWU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.46gYzA8BqZarsSismrtiSK-mV5olei9Q30Xvvlpwo5A'
-    }
-  };
-
-  useEffect(() => {
-    fetch('https://api.themoviedb.org/3/trending/movie/week?language=en-US', options)
-    .then(response => response.json())
-    .then(data => {
-      setMovie(data.results);
-    })
-    .catch(err => console.error(err));
-  }, []);
 
   
   return (
@@ -46,15 +27,7 @@ function AppRouter() {
         </Routes>
         </Nav>
         <Banner />
-        <ul>
-            {movies.map((movie) => (
-              <li key={movie.id}>{movie.title}
-              <img  key={movie.id} src={"https://image.tmdb.org/t/p/w185/" + movie.poster_path}>
-              </img>
-              </li>
-            ))}
-        </ul>
-
+        <HomeAPI />
         <Footer />
       </div>
     </BrowserRouter>
