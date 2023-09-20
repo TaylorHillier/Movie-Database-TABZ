0c699f67ed21fd6956a76950502d82981dea7e0a
Zeinab Kordeh <zeinabkordeh@gmail.com>
2023-09-20 09:26:08 -0700

    add hover

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,65 +1,45 @@
 import { useLocation } from "react-router-dom";
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import 'swiper/css/navigation';
 import "./App.css";
 import PopAPI from "../api/APIFunctionality";
-import { Pagination, Autoplay , Navigation} from "swiper/modules";
-
-function Banner() {
-  const location = useLocation();
-  const movies = PopAPI('popular');
-  const isLandingPage = location.pathname === "/";
-  const fourmovies = movies.slice(0, 4);
-  const twelvemovies = movies.slice(0, 12);
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
 
 
-  return (
-    <>
-      {isLandingPage && (
-        <Swiper
-          spaceBetween={30}
-          autoplay={{
-            delay: 1500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination, Autoplay]}
-          className="mySwiper"
-        >
-          {fourmovies.map((movie) => (
-            <SwiperSlide key={movie.id}>
-              <img
-                className="imgBanner"
-                src={"https://image.tmdb.org/t/p/w1280/" + movie.backdrop_path}
-                alt={movie.title}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
-    </>
-  );
+function truncateText(text, maxWords) {
+  const words = text.split(" ");
+  if (words.length <= maxWords) {
+    return text;
+  } else {
+    const truncatedText = words.slice(0, maxWords).join(" ");
+    return truncatedText + " ...";
+  }
 }
 
-function AdditionalSlider({ twelvemovies }) {
+function BannerSlider({ movies }) {
+  const fiveMovies = movies.slice(0, 5);
+
   return (
     <Swiper
-      slidesPerView={6}
-      spaceBetween={0}
-      navigation={true}
-      modules={[ Navigation]}
+      spaceBetween={30}
+      autoplay={{
+        delay: 1500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination, Autoplay]}
       className="mySwiper"
     >
-      {twelvemovies.map((movie) => (
+      {fiveMovies.map((movie) => (
         <SwiperSlide key={movie.id}>
           <img
-            className="imgBanner-pop"
-            src={"https://image.tmdb.org/t/p/w1280/" + movie.poster_path}
+            className="imgBanner"
+            src={"https://image.tmdb.org/t/p/w1280/" + movie.backdrop_path}
             alt={movie.title}
           />
         </SwiperSlide>
@@ -68,14 +48,59 @@ function AdditionalSlider({ twelvemovies }) {
   );
 }
 
+function AdditionalSlider({ twelvemovies }) {
+  return (
+    <Swiper
+      slidesPerView={5}
+      spaceBetween={0}
+      navigation={true}
+      modules={[Navigation]}
+      className="mySwiper"
+    >
+      {twelvemovies.map((movie) => (
+        <SwiperSlide key={movie.id}>
+          <div className="imgBanner-pop">
+            <img
+              src={"https://image.tmdb.org/t/p/w1280/" + movie.poster_path}
+              alt={movie.title}
+              className="movie-poster"
+            />
+            <div className="movie-info">
+              <p>{movie.vote_average}</p>
+              <h3>{movie.title}</h3>
+              <p>{truncateText(movie.overview, 30)}</p>
+              <button className="readMore">
+                <a href="#">Read More</a>
+              </button>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
 export default function App() {
-  const movies = PopAPI('popular');
-  const twelvemovies = movies.slice(0, 12);
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/";
+
+  const popularMovies = PopAPI('popular');
+  const upcomingMovies = PopAPI('upcoming');
+  const topRatedMovies = PopAPI('top_rated');
+  const nowPlayingMovies = PopAPI('now_playing');
+  const twelvemovies = popularMovies.slice(0, 12);
 
   return (
     <div>
-      <Banner />
-      <AdditionalSlider twelvemovies={twelvemovies} />
+      {isLandingPage && (
+        <>
+          <BannerSlider movies={popularMovies} />
+          <AdditionalSlider twelvemovies={twelvemovies} />
+          <AdditionalSlider twelvemovies={upcomingMovies} />
+          <AdditionalSlider twelvemovies={topRatedMovies} />
+          <AdditionalSlider twelvemovies={nowPlayingMovies} />
+        </>
+      )}
     </div>
   );
 }
