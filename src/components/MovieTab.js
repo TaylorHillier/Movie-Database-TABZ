60ac79cfd0c01df049866012d504fad02ebc0cc8
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2023-09-30 20:55:19 -0700

    Rename MovieTab 'movie' parameter

--- src/components/MovieTab.js
+++ src/components/MovieTab.js
@@ -1,18 +1,18 @@
 // Individual Movie Poster Tabs
 
-export default function MovieTab({ movie }) {
-    const releaseDate = new Date(movie.release_date).toDateString();
+export default function MovieTab({ movieObj }) {
+    const releaseDate = new Date(movieObj.release_date).toDateString();
     const releaseDateString = releaseDate.slice(4, releaseDate.length);
 
     return (
         <>
-            <div key={movie.id} className="w-fit">
+            <div key={movieObj.id} className="w-fit">
                 <img
                     className="w-1/5"
-                    src={"https://image.tmdb.org/t/p/original/" + movie.poster_path}
+                    src={"https://image.tmdb.org/t/p/original/" + movieObj.poster_path}
                 />
-                <h3 className="uppercase text-gray-600 mt-2">{movie.title}</h3>
-                <p className="text-neutral-50">{movie.overview}</p>
+                <h3 className="uppercase text-gray-600 mt-2">{movieObj.title}</h3>
+                <p className="text-neutral-50">{movieObj.overview}</p>
                 <b>{releaseDateString}</b>
             </div>
         </>
