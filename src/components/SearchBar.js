634a13eef951b7d606af8d844f40585b602fd013
Taylor Hillier <hilliertaylor7@gmail.com>
2023-10-12 12:03:47 -0700

    formatting for search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -50,7 +50,9 @@ useEffect(() => {
   return (
       
     <div>
+        <svg className="relative left-8" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z" fill='black'/></svg>
       <input
+      className='p-4 w-4/5 m-auto'
         type="text"
         placeholder="Search for movies..."
         value={query}
@@ -58,14 +60,17 @@ useEffect(() => {
       />
       {
         movies.map((movie, index) => (
-            <div key={index}>
+            <div key={index} className='grid grid-cols-[1fr_9fr]'>
                 <img
-            key={index}
-            src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`}
-            alt={movie.original_title + " backdrop image"}
-          />
-                <h2>{movie.title}</h2>
+                key={index}
+                src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
+                alt={movie.original_title + " backdrop image"}
+                />
+                <div className=''>
+                <h2 className='p-2'>{movie.title} ({movie.release_date})</h2>
+                <p className='p-2'>{movie.overview}</p>
                 </div>
+            </div>
         ))
         }
     </div>
