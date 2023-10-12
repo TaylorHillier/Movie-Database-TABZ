b854ac9aacd7151c94376f575bfa82c42a2acdc9
Bruce Gerona <brucegerona@yahoo.com>
2023-10-12 10:02:37 -0700

    added navigation links on the footer and created a scroll to top button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import ScrollToTopButton from "./ScrollToTopButton.js";
 
 function Footer() {
     return (
@@ -8,8 +9,16 @@ function Footer() {
                 <div className="footer-content">
                     <p>&copy; 2023 TABZ Movie Database</p>
                     <p>Contact us: contact@example.com</p>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/favorites">Favorites</a></li>
+                            <li><a href="/about">About</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </footer>
+            <ScrollToTopButton />
         </>
     );
 }
