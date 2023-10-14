c01d4a65bddcd9063ab2a47b6757ff46b57b9826
TaylorHillier <134565093+TaylorHillier@users.noreply.github.com>
2023-10-14 14:41:10 -0700

    re-styling after issues

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,34 +8,34 @@ function SearchBar(resetSearch) {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
 
-    const accessToken =
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjI3MGZmZmVkNjliYzFkNDdkZTMyNjQ4ZmYwNTBjZCIsInN1YiI6IjY0ZWQwMzNjYzYxM2NlMDEyY2M2YWU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.46gYzA8BqZarsSismrtiSK-mV5olei9Q30Xvvlpwo5A";
+  const accessToken =
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjI3MGZmZmVkNjliYzFkNDdkZTMyNjQ4ZmYwNTBjZCIsInN1YiI6IjY0ZWQwMzNjYzYxM2NlMDEyY2M2YWU5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.46gYzA8BqZarsSismrtiSK-mV5olei9Q30Xvvlpwo5A";
 
-    const apiKey = "ef270fffed69bc1d47de32648ff050cd";
+  const apiKey = "ef270fffed69bc1d47de32648ff050cd";
 
-    const authHeader = {
-        method: "GET",
-        headers: {
-            accept: "application/json",
-            Authorization: accessToken,
-        },
-    };
+  const authHeader = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  };
 
   const fetchSearch = async (query) => {
     try {
-        const response = await fetch(
-            `https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`,
-            authHeader
-        );
-        let data = await response.json();
-        console.log("Searching");
-        return data.results;
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`,
+        authHeader
+      );
+      let data = await response.json();
+      console.log("Searching");
+      return data.results;
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-};
+  };
 
-useEffect(() => {
+  useEffect(() => {
     async function fetchData() {
       if (query.trim() === '') {
         setMovies([]); // No query, clear the movie list
@@ -44,62 +44,61 @@ useEffect(() => {
         setMovies(results);
       }
     }
-  
+
     fetchData();
   }, [query]);
-  
+
 
   console.log('Query:', query);
   console.log('Movies:', movies);
 
   function formatDate(APIDate) {
     const date = new Date(APIDate);
-    const options = { year: 'numeric'};
+    const options = { year: 'numeric' };
     return date.toLocaleDateString('en-US', options);
   }
 
   function shortenText(text, maxLength) {
     if (text.length <= maxLength) {
-        return text;
+      return text;
     }
     return text.slice(0, maxLength) + '...';
-}
+  }
 
   return (
-      
+
     <div>
       <div className='search-bar flex m-8 z-100'>
-        <svg className="" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z" fill='black'/></svg>
         <input
-        className='p-4 w-3/5 m-auto'
+          className='p-4 m-auto w-full bg-slate-900 text-white border-white'
           type="text"
           placeholder="Search for movies..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
       </div>
-      <section className={`${query ? 'visible' : 'hidden'} search-area backdrop-blur backdrop-brightness-50 absolute m-auto flex w-full z-50 h-max ${style['animated-section']}`}>
+      <section className={`${query ? 'visible' : 'hidden'} search-area backdrop-blur backdrop-brightness-50 absolute m-auto flex w-full z-50 ${style['animated-section']}`}>
         {movies.length > 0 ? (
-        <div className='results-container m-auto md:w-3/5'>
-          {query && (
-          <h1 className='ml-4 md:ml-0' >Closest matches for: {query}</h1>
-          )}
-          
-          <div className='grid-cols-2 grid grid-cols-2 gap-4 md:grid-cols-1 md:gap-0 md:bg-theme-bg search-results w-full h-full p-4 rounded-lg shadow-lg'>
+          <div className='results-container m-auto md:w-3/5'>
+            {query && (
+              <h1 className='ml-4 md:ml-0' >Closest matches for: {query}</h1>
+            )}
+
+            <div className='grid-cols-2 grid grid-cols-2 gap-4 md:grid-cols-1 md:gap-0 md:bg-theme-bg search-results w-full h-full p-4 rounded-lg shadow-lg'>
               {
 
-              movies.slice(0, 12).map((movie, index) => (
+                movies.slice(0, 12).map((movie, index) => (
 
-                <div key={index} className='grid grid-row-[10fr-1fr] md:grid-cols-[3fr_17fr] m-auto mb-4 md:border-solid md:p-2'>
-                  
-                   <Link to={`/movie/${movie.id}`} onClick={resetSearch}>
-                   <img
-                      key={index}
-                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                      alt={movie.original_title + " backdrop image"}
-                      className='mb-2 object-cover rounded-md'
+                  <div key={index} className='grid grid-row-[10fr-1fr] md:grid-cols-[3fr_17fr] m-auto mb-4 md:border-solid md:p-2'>
+
+                    <Link to={`/movie/${movie.id}`} onClick={resetSearch}>
+                      <img
+                        key={index}
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.original_title + " backdrop image"}
+                        className='mb-2 object-cover rounded-md'
                       />
-                </Link>   
+                    </Link>
 
                     <div className='search-info justify-between md:block'>
 
@@ -107,7 +106,7 @@ useEffect(() => {
 
                         <div className='movie-title flex flex-col md:flex-row md:items-center mb-2 md:mb-0'>
 
-                        <Link to={`/movie/${movie.id}`} onClick={resetSearch}><h4 className='md:p-2 mb-2  md:mb-0'>{movie.title} </h4> </Link>  <p>({formatDate(movie.release_date)})</p>
+                          <Link to={`/movie/${movie.id}`} onClick={resetSearch}><h3 className='md:p-2 mb-2  md:mb-0'>{movie.title} </h3> </Link>  <p>({formatDate(movie.release_date)})</p>
 
                         </div>
                         <div className='rating'>
@@ -123,19 +122,19 @@ useEffect(() => {
                     </div>
 
                   </div>
-                  
-                
-                  ))}
-              
-          </div>
-        </div> ) : (
+
+
+                ))}
+
+            </div>
+          </div>) : (
           setTimeout(() => {
             <p className='w-full text-center p-12 h-full'>{query ? 'No Movies found.' : 'Start Typing to find movies.'}</p>
           }, 1000)
         )}
       </section>
     </div>
-    );
+  );
 }
 
 export default SearchBar;
--- src/pages/PageLogin.js
+++ src/pages/PageLogin.js
@@ -2,13 +2,14 @@ import { UserContext } from "../context/userState";
 import { useContext } from "react";
 import { useState } from "react";
 import { FavoriteContext } from "../context/movieState";
+import SearchBar from "../components/SearchBar";
 
 function PageLogin() {
 
     const { user, createUser, deleteUser } = useContext(UserContext);
     const [newUser, setNewUser] = useState("");
     const [error, setError] = useState("");
-
+    const { favorites } = useContext(FavoriteContext);
 
     function handleLogIn() {
         if (newUser.trim() === "") {
@@ -24,12 +25,20 @@ function PageLogin() {
     }
 
     return (
-        <div className="w-4/5 m-auto">
+        <div className=" md:m-auto">
             {user ? (
                 <div>
-                    <h1>Welcome, {user.name}!</h1>
-                    <p>You currently have {}</p>
-                    <button onClick={handleLogOut} className="p-2 my-4 bg-theme-red text-white border-none">Logout</button>
+                    <h1 className="ml-4">Welcome, {user.name}!</h1>
+                    <p className="ml-4 mb-4">You currently have {favorites.length} favorited movies.</p>
+                    {favorites.length > 0 ? (
+                        <p>Checkout your favorites <a href="">Here</a></p>
+                    ) : (
+                        <div className="search">
+                            <p className="ml-4">Explore our database below.</p>
+                            <SearchBar />
+                        </div>
+                    )}
+                    <button onClick={handleLogOut} className="p-2 my-4 bg-theme-red text-white border-none ml-4">Logout</button>
                 </div>
             ) : (
                 <div className="flex m-auto">
@@ -49,7 +58,7 @@ function PageLogin() {
                         {error && <p style={{ color: "red" }}>{error}</p>}
                     </div>
                     <h1 className="p-8">Welcome! Log in here to view your favorites, account details and more...</h1>
-                    
+
                 </div>
             )}
         </div>
